cdd5680ff45f5b4341dff1798334e94b808e8c8a
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-21 13:17:23 -0600

    moved setup react func
    to index.tsx to remove clutter

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,7 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-import { setupIonicReact } from '@ionic/react';
-setupIonicReact();
+
 
 const App = () => {
   return (

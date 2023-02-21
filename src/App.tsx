4dea6f42d0de83509edb4d51be02c04523d9033d
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-21 13:12:59 -0600

    Added ionic in correct place and
     the code for making the app npx create-react-app farkle-companion-app --template typescript
    and ionic cli install code npm i @ionic/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+
 import { 
   IonCard, 
   IonCardContent, 
@@ -8,6 +8,26 @@ import {
   IonCardSubtitle, 
   IonCardTitle, 
   IonDatetime } from '@ionic/react';
+
+import './App.css';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css'; // Remove if nothing is visible
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+import { setupIonicReact } from '@ionic/react';
+setupIonicReact();
 
 const App = () => {
   return (
